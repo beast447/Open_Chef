f7bd05aa0aba50235f4f040f719f8b5eb655199f
Trevor King <wildbeast447@gmail.com>
2026-03-12 16:55:36 -0400

    improved input box width for mobile devices

--- app/page.tsx
+++ app/page.tsx
@@ -79,7 +79,7 @@ function newRecipe() {
       <form onSubmit={handleSubmit}>
         <input
           required
-          className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-red-500 w-full max-w-lg"
           name="ingredients"
           type="text"
           placeholder="Apples, Chicken, Bacon..."
